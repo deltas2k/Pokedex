f351f65b6771da66c8cb2c79e91481a1df056935
Matthew O <deltas2k@gmail.com>
2019-10-01 11:07:22 -0600

    model commit

---
+++ Pokedex/Pokedex/models/pokemon.swift
@@ -0,0 +1,40 @@
+//
+//  pokemon.swift
+//  Pokedex
+//
+//  Created by Matthew O'Connor on 10/1/19.
+//  Copyright © 2019 Matthew O'Connor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct TopLevelDictionary: Decodable {
+    private enum CodingKeys: String, CodingKey { //naming specific, must be exhaustive
+        case abilities
+        case name
+        case id
+        case spriteDict = "sprites"
+        
+    }
+    
+    let name: String
+    let id: Int
+    let spriteDict: SpriteDict
+    let abilites: [AbilityDict]
+}
+
+struct SpriteDict: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case image = "front_shiny"
+    }
+    let image: URL
+}
+
+struct AbilityDict: Decodable {
+    let ability: Ability
+}
+
+struct Ability: Decodable {
+    let name: String
+}
